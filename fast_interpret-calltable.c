7a4f2095287dc0fc510a7bfa17593b31bdc67959
Krüpl Zsolt <hg2ecz@ham.hu>
2016-11-19 09:05:16 +0100

    faster vcpu

--- fast_interpret-calltable.c
+++ fast_interpret-calltable.c
@@ -1,47 +1,48 @@
+#include <limits.h>
 #include "fast_interpret.h"
 
-static struct _vcpu {
-    int progct;
-    _Bool run;
-} vcpu;
+static int instr_nop(double *out, double m1, double m2) { return 0; }
 
-static void instr_nop(double *out, double m1, double m2) {}
-
-static void instr_and(double *out, double m1, double m2) {
+static int instr_and(double *out, double m1, double m2) {
     int m1_i = m1;
     int m2_i = m2;
     *out = m1_i & m2_i;
+    return 0;
 }
-static void instr_or (double *out, double m1, double m2) {
+static int instr_or (double *out, double m1, double m2) {
     int m1_i = m1;
     int m2_i = m2;
     *out = m1_i | m2_i;
+    return 0;
 }
-static void instr_xor(double *out, double m1, double m2) {
+static int instr_xor(double *out, double m1, double m2) {
     int m1_i = m1;
     int m2_i = m2;
     *out = m1_i ^ m2_i;
+    return 0;
 }
-static void instr_not(double *out, double m1, double m2) {
+static int instr_not(double *out, double m1, double m2) {
     int m1_i = m1;
     *out = ~m1_i;
+    return 0;
 }
 
-static void instr_add(double *out, double m1, double m2) {*out = m1 + m2; }
-static void instr_sub(double *out, double m1, double m2) {*out = m1 - m2; }
-static void instr_mul(double *out, double m1, double m2) {*out = m1 * m2; }
-static void instr_div(double *out, double m1, double m2) {*out = m1 / m2; }
+static int instr_add(double *out, double m1, double m2) {*out = m1 + m2; return 0; }
+static int instr_sub(double *out, double m1, double m2) {*out = m1 - m2; return 0; }
+static int instr_mul(double *out, double m1, double m2) {*out = m1 * m2; return 0; }
+static int instr_div(double *out, double m1, double m2) {*out = m1 / m2; return 0; }
 
-static void instr_jnz(double *out, double m1, double m2) {
-    if (m1) vcpu.progct+=m2;
+static int instr_jnz(double *out, double m1, double m2) {
+    if (m1) return m2;
+    return 0;
 }
 
-static void instr_exit(double *out, double m1, double m2) {
-    vcpu.run=0;
+static int instr_exit(double *out, double m1, double m2) {
+    return INT_MIN;
 }
 
 
-static void (*instructions[])(double *out, double m1, double m2) = {
+static int (*instructions[])(double *out, double m1, double m2) = {
     instr_nop,
     instr_and,
     instr_or,
@@ -64,14 +65,13 @@ static void (*instructions[])(double *out, double m1, double m2) = {
 };
 
 void interpret_calltable(struct _vliw *vliw, int len) {
-    vcpu.progct=0;
-    vcpu.run=1;
-    while (vcpu.run) {
-	instructions[vliw[vcpu.progct].opcode & 0x0f](
-		vliw[vcpu.progct].outptr,
-		*vliw[vcpu.progct].m1ptr,
-		*vliw[vcpu.progct].m2ptr
+    int progct=0;
+    while (progct >= 0) {
+	progct+=instructions[vliw[progct].opcode & 0x0f](
+		vliw[progct].outptr,
+		*vliw[progct].m1ptr,
+		*vliw[progct].m2ptr
 	);
-	vcpu.progct++;
+	progct++;
     }
 }
