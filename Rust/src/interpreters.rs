a7d18b30319fb73ca0df70d2ca1deb0c5fd7fd23
Zsolt Krüpl <hg2ecz@ham.hu>
2018-04-25 20:46:15 +0000

    usize problem on sys32

--- Rust/src/interpreters.rs
+++ Rust/src/interpreters.rs
@@ -32,12 +32,11 @@ pub enum VliwEnum {
 }
 
 pub fn interpret_switch(vliw_enum: Vec<VliwEnum>, mut reg: Vec<f64>) -> Vec<f64> {
-    let invalid = isize::min_value();
-    let mut callstack: Vec<isize> = vec![invalid];
-    let mut progct: isize=0;
-    while progct >= 0 {
-	let pct = progct as usize;
-	match vliw_enum[pct] { // hint: NOP <== and 0, 0, 0  ( out = reg0 & reg0 )
+    let invalid = usize::max_value()/2;
+    let mut callstack: Vec<usize> = vec![invalid];
+    let mut progct: usize=0;
+    while progct < invalid {
+	match vliw_enum[progct] { // hint: NOP <== and 0, 0, 0  ( out = reg0 & reg0 )
 	    VliwEnum::And (outptr, m1ptr, m2ptr)  => reg[outptr] = (reg[m1ptr] as u64 & reg[m2ptr] as u64) as f64,
 	    VliwEnum::Or  (outptr, m1ptr, m2ptr)  => reg[outptr] = (reg[m1ptr] as u64 | reg[m2ptr] as u64) as f64,
 	    VliwEnum::Xor (outptr, m1ptr, m2ptr)  => reg[outptr] = (reg[m1ptr] as u64 ^ reg[m2ptr] as u64) as f64,
@@ -49,15 +48,15 @@ pub fn interpret_switch(vliw_enum: Vec<VliwEnum>, mut reg: Vec<f64>) -> Vec<f64>
 
 	    VliwEnum::Div   (outptr, m1ptr, m2ptr)=> reg[outptr] = reg[m1ptr] / reg[m2ptr],
 	    VliwEnum::Mod   (outptr, m1ptr, m2ptr)=> reg[outptr] = reg[m1ptr] % reg[m2ptr],
-	    VliwEnum::Incjl (outptr, m1ptr, m2ptr)=> {reg[outptr]+=1.; if reg[outptr] <  reg[m2ptr] {progct+=reg[m1ptr] as isize}},
-	    VliwEnum::Decjge(outptr, m1ptr, m2ptr)=> {reg[outptr]-=1.; if reg[outptr] >= reg[m2ptr] {progct+=reg[m1ptr] as isize}},
+	    VliwEnum::Incjl (outptr, m1ptr, m2ptr)=> {reg[outptr]+=1.; if reg[outptr] <  reg[m2ptr] {progct+=reg[m1ptr] as isize as usize}},
+	    VliwEnum::Decjge(outptr, m1ptr, m2ptr)=> {reg[outptr]-=1.; if reg[outptr] >= reg[m2ptr] {progct+=reg[m1ptr] as isize as usize}},
 
-	    VliwEnum::Je    (outptr, m1ptr, m2ptr)=> if reg[outptr] == reg[m2ptr] {progct+=reg[m1ptr] as isize},
-	    VliwEnum::Jne   (outptr, m1ptr, m2ptr)=> if reg[outptr] != reg[m2ptr] {progct+=reg[m1ptr] as isize},
+	    VliwEnum::Je    (outptr, m1ptr, m2ptr)=> if reg[outptr] == reg[m2ptr] {progct+=reg[m1ptr] as isize as usize},
+	    VliwEnum::Jne   (outptr, m1ptr, m2ptr)=> if reg[outptr] != reg[m2ptr] {progct+=reg[m1ptr] as isize as usize},
 
 	    VliwEnum::Call  (_outptr, m1ptr, m2ptr)=> {
-		callstack.push(reg[m2ptr] as isize);
-		progct += reg[m1ptr] as isize;
+		callstack.push(m2ptr);
+		progct += m1ptr;
 	    },
 	    VliwEnum::Ret   (_outptr, _m1ptr, _m2ptr)=> progct += callstack.pop().unwrap(), // return (last return --> exit)
 	}
@@ -68,23 +67,23 @@ pub fn interpret_switch(vliw_enum: Vec<VliwEnum>, mut reg: Vec<f64>) -> Vec<f64>
 
 // == Calltable ==
 
-fn instr_and(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { ((m1 as u64 & m2 as u64) as f64, 0) }
-fn instr_or (_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { ((m1 as u64 | m2 as u64) as f64, 0) }
-fn instr_xor(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { ((m1 as u64 ^ m2 as u64) as f64, 0) }
-fn instr_shl(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (((m1 as u64) << m2 as u64) as f64, 0) }
-fn instr_shr(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (((m1 as u64) >> m2 as u64) as f64, 0) }
-fn instr_add(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (m1 + m2, 0) }
-fn instr_sub(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (m1 - m2, 0) }
-fn instr_mul(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (m1 * m2, 0) }
-fn instr_div(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (m1 / m2, 0) }
-fn instr_mod(_callstack: &mut Vec<isize>, _out:f64, m1: f64, m2: f64) -> (f64, isize) { (m1 % m2, 0) }
+fn instr_and(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { ((m1 as u64 & m2 as u64) as f64, 0) }
+fn instr_or (_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { ((m1 as u64 | m2 as u64) as f64, 0) }
+fn instr_xor(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { ((m1 as u64 ^ m2 as u64) as f64, 0) }
+fn instr_shl(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (((m1 as u64) << m2 as u64) as f64, 0) }
+fn instr_shr(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (((m1 as u64) >> m2 as u64) as f64, 0) }
+fn instr_add(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (m1 + m2, 0) }
+fn instr_sub(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (m1 - m2, 0) }
+fn instr_mul(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (m1 * m2, 0) }
+fn instr_div(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (m1 / m2, 0) }
+fn instr_mod(_callstack: &mut Vec<usize>, _out:f64, m1: f64, m2: f64) -> (f64, usize) { (m1 % m2, 0) }
 
-fn instr_incjl (_callstack: &mut Vec<isize>, out:f64, m1: f64, m2: f64) -> (f64, isize) { let o = out+1.; if o < m2 { (o, m1 as isize) } else {(o, 0)} }
-fn instr_decjge(_callstack: &mut Vec<isize>, out:f64, m1: f64, m2: f64) -> (f64, isize) { let o = out-1.; if o >= m2 { (o, m1 as isize) } else {(o, 0)} }
-fn instr_je    (_callstack: &mut Vec<isize>, out:f64, m1: f64, m2: f64) -> (f64, isize) { if out == m2 { (out, m1 as isize) } else { (out, 0)} }
-fn instr_jne   (_callstack: &mut Vec<isize>, out:f64, m1: f64, m2: f64) -> (f64, isize) { if out != m2 { (out, m1 as isize) } else { (out, 0)} }
-fn instr_call(callstack: &mut Vec<isize>, out:f64, m1: f64, m2: f64) -> (f64, isize) { callstack.push(m2 as isize); (out, m1 as isize) }
-fn instr_ret(callstack: &mut Vec<isize>, out:f64, _m1: f64, _m2: f64) -> (f64, isize) { (out, callstack.pop().unwrap()) }
+fn instr_incjl (_callstack: &mut Vec<usize>, out:f64, m1: f64, m2: f64) -> (f64, usize) { let o = out+1.; if o < m2 { (o, m1 as isize as usize) } else {(o, 0)} }
+fn instr_decjge(_callstack: &mut Vec<usize>, out:f64, m1: f64, m2: f64) -> (f64, usize) { let o = out-1.; if o >= m2 { (o, m1 as isize as usize) } else {(o, 0)} }
+fn instr_je    (_callstack: &mut Vec<usize>, out:f64, m1: f64, m2: f64) -> (f64, usize) { if out == m2 { (out, m1 as isize as usize) } else { (out, 0)} }
+fn instr_jne   (_callstack: &mut Vec<usize>, out:f64, m1: f64, m2: f64) -> (f64, usize) { if out != m2 { (out, m1 as isize as usize) } else { (out, 0)} }
+fn instr_call(callstack: &mut Vec<usize>, out:f64, m1: f64, m2: f64) -> (f64, usize) { callstack.push(m2 as usize); (out, m1 as usize) }
+fn instr_ret(callstack: &mut Vec<usize>, out:f64, _m1: f64, _m2: f64) -> (f64, usize) { (out, callstack.pop().unwrap()) }
 
 pub struct VliwStruct (
     pub usize,
@@ -94,7 +93,7 @@ pub struct VliwStruct (
 );
 
 pub fn interpret_calltable(vliw_struct: Vec<VliwStruct>, mut reg: Vec<f64>) -> Vec<f64> {
-    let instructions: Vec<fn(callstack: &mut Vec<isize>, out:f64, m1: f64, m2: f64) -> (f64, isize)> = vec![
+    let instructions: Vec<fn(callstack: &mut Vec<usize>, out:f64, m1: f64, m2: f64) -> (f64, usize)> = vec![
 	instr_and,
 	instr_or,
 	instr_xor,
@@ -114,14 +113,13 @@ pub fn interpret_calltable(vliw_struct: Vec<VliwStruct>, mut reg: Vec<f64>) -> V
 	instr_ret
     ];
 
-    let invalid = isize::min_value();
-    let mut callstack: Vec<isize>=vec![invalid];
-    let mut progct: isize=0;
+    let invalid = usize::max_value()/2;
+    let mut callstack: Vec<usize>=vec![invalid];
+    let mut progct: usize=0;
 
-    while progct >= 0 {
-	let pct = progct as usize;
-	let res=instructions[vliw_struct[pct].0 & 0x0f](&mut callstack, reg[vliw_struct[pct].1], reg[vliw_struct[pct].2], reg[vliw_struct[pct].3]);
-	reg[vliw_struct[pct].1] = res.0; progct+=res.1;
+    while progct < invalid {
+	let res=instructions[vliw_struct[progct].0 & 0x0f](&mut callstack, reg[vliw_struct[progct].1], reg[vliw_struct[progct].2], reg[vliw_struct[progct].3]);
+	reg[vliw_struct[progct].1] = res.0; progct+=res.1;
 	progct+=1;
     }
     return reg;
