16af4c8100ceb01fa05f865ef7c3101331c31afd
Zsolt Krüpl <hg2ecz@ham.hu>
2023-07-04 12:23:32 +0200

    Very basic examples

--- Very_basic_examples/computed_goto_fast.c
+++ Very_basic_examples/computed_goto_fast.c
@@ -1,9 +1,11 @@
-// Assembly code: https://c.godbolt.org/z/aoGhTfK5r
+// Assembly code: https://c.godbolt.org/z/Ksj8sb6z6
 #include <stddef.h>
 #include <stdint.h>
 
+#define PROGLEN 0x10000
+
 // Opcodes need prefiltering between 0..7, so no runtime check here.
-int32_t interpret_goto_example(const uint8_t *opcodes) {
+int32_t interpret_example(unsigned char opcodes[PROGLEN]) {
     size_t instrptr=0;
     int32_t reg=0;
 
@@ -20,24 +22,31 @@ int32_t interpret_goto_example(const uint8_t *opcodes) {
 
     add_133:
                 reg += 133;
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     sub_133:
                 reg -= 133;
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     add_155:
                 reg += 155;
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     sub_155:
                 reg -= 155;
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     add_177:
                 reg += 177;
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     sub_177:
                 reg -= 177;
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     reg_to_pc:
                 instrptr = reg;                    // JMP
+                instrptr %= PROGLEN;
                 goto *inst_t[opcodes[instrptr++]];
     ret:
                 return reg;
